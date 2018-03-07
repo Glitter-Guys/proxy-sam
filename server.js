57dc37a43a1daad548a3c47df39d381d2292d67f
Sam Watson <Sam.Watson3775@gmail.com>
2018-03-06 16:58:14 -0800

    Fix api route call for service-attendees

--- server.js
+++ server.js
@@ -3,11 +3,15 @@ const morgan = require('morgan');
 const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
+const cors = require('cors')
 
+
+app.use(cors());
 app.use(morgan('dev'));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use('/event/:eventid', express.static(path.join(__dirname, 'public')));
 
 app.listen(port, () => {
-  console.log(`server running at: http://localhost:${port}`)
+  console.log(`proxy server running at: http://localhost:${port}`)
 });
+
 
